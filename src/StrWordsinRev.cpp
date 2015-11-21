f2dd202f44689043caebc622ebd03efe8ad11f51
Divya37 <undidivya1996@gmail.com>
2015-11-21 11:22:57 +0500

    Submit Commit

--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -11,7 +11,43 @@ NOTES: Don't create new string.
 */
 #include <Stdio.h>
 #include <string.h>
+void str_words_in_rev(char *input, int len)
+{
+	int i = 0, j = 0, index, space_index = 0, k;
+	char temp;
+	for (i = 0; i < len; i++)
+	{
+		if (input[i] == ' ')
+			j++;
+	}
+	if (j > 0)
+	{
+		for (i = 0, k = len - 1; i <= k; i++, k--)
+		{
+			temp = input[i];
+			input[i] = input[k];
+			input[k] = temp;
+		}
+		for (i = 0; i <= len; i++)
+		{
+			if (input[i] == ' ' || input[i] == '\0')
+			{
+				if (i>0)
+				{
+					if (space_index - i != 1)
+					{
+						for (j = space_index, index = i - 1; j <= index; j++, index--)
+						{
+							temp = input[index];
+							input[index] = input[j];
+							input[j] = temp;
+						}
 
-void str_words_in_rev(char *input, int len){
+					}
+				}
+				space_index = i + 1;
+			}
+		}
+	}
 	
 }
